933db641c2b5f51c50d0f87c05a6093de6044487
Danylo Sokol <danylosokol@Mac.lan>
2026-06-10 08:01:19 +0100

    feat: parse tests

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -19,25 +19,29 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	indx := bytes.Index(data, []byte(crlf))
 	if indx == -1 {
 		return 0, false, nil
-	} 
+	}
 
 	if indx == 0 {
-		return 0, true, nil
+		return 2, true, nil
 	}
 
 	headerLineParts := strings.SplitN(string(data[:indx]), ":", 2)
 	if len(headerLineParts) != 2 {
 		return 0, false, fmt.Errorf("Expected to have key and a value split by :")
 	}
-	
+
 	key := headerLineParts[0]
-	if len(strings.TrimSpace(key)) != len(key) {
+	if strings.TrimSpace(key) != key {
 		return 0, false, fmt.Errorf("Invalid spacing header")
 	}
-	
+
 	value := strings.TrimSpace(headerLineParts[1])
-	
-	h[key] = value
-	
+
+	h.Set(key, value)
+
 	return len(data[:indx]) + 2, false, nil
+}
+
+func (h Headers) Set(key, value string) {
+	h[key] = value
 }
