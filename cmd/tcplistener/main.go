cca99bbd7749ca80481a6eb31c00f93145036087
Danylo Sokol <danylosokol@Mac.lan>
2026-07-10 08:41:57 +0100

    feat: update output in main

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -34,6 +34,10 @@ func main() {
 		fmt.Printf("- Method: %s\n", request.RequestLine.Method)
 		fmt.Printf("- Target: %s\n", request.RequestLine.RequestTarget)
 		fmt.Printf("- Version: %s\n", request.RequestLine.HttpVersion)
+		fmt.Printf("Headers:\n")
+		for key, value := range request.Headers {
+			fmt.Printf("- %s: %s\n", key, value)
+		}
 		fmt.Printf("connection to %s closed", conn.RemoteAddr())
 	}
 }
